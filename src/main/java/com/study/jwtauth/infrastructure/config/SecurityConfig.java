ccf0897b21007909301508aa2142e0554dff3336
0ssang <dimple0513@naver.com>
2025-10-15 18:30:00 +0900

    fix: filterChain 메서드명 수정

--- src/main/java/com/study/jwtauth/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/study/jwtauth/infrastructure/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
