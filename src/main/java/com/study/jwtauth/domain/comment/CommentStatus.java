85c7e191e634327146e250409c559a9f4be164a8
0ssang <dimple0513@naver.com>
2025-11-22 00:27:37 +0900

    feat: CommentStatus 상태값 구현

--- src/main/java/com/study/jwtauth/domain/comment/CommentStatus.java
+++ src/main/java/com/study/jwtauth/domain/comment/CommentStatus.java
@@ -1,4 +1,15 @@
 package com.study.jwtauth.domain.comment;
 
+import lombok.Getter;
+
+@Getter
 public enum CommentStatus {
+    ACTIVE("활성"),
+    DELETED("삭제됨");
+
+    private final String description;
+
+    CommentStatus(String description) {
+        this.description = description;
+    }
 }
